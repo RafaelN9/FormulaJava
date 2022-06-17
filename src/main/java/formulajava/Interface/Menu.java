fc249bd9abec319de23d4ceb6ed8dafde505cc7f
RafaelN9 <rafael.n.correia@unesp.br>
2022-06-16 22:12:54 -0300

    Interface Updated ( Tabela)

--- src/main/java/formulajava/Interface/Menu.java
+++ src/main/java/formulajava/Interface/Menu.java
@@ -12,6 +12,10 @@ import formulajava.Implementaocao.Corrida;
  */
 public class Menu extends javax.swing.JFrame {
 
+    public static void main(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
     /**
      * Creates new form Menu
      */
